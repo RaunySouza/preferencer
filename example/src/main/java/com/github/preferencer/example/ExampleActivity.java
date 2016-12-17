60630c703ebf77c6c8c2a0ed05a656797c62d835
Rauny Souza <rauny.souza@gmail.com>
2016-12-17 17:38:10 -0300

    Adds Example Activity that shows main features

---
+++ example/src/main/java/com/github/preferencer/example/ExampleActivity.java
@@ -0,0 +1,38 @@
+package com.github.preferencer.example;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Set;
+
+/**
+ * @author raunysouza
+ */
+public class ExampleActivity extends AppCompatActivity {
+
+    private SettingsPreference mSettingsPreference;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mSettingsPreference = SettingsPreference.getInstance(this);
+
+        mSettingsPreference.name()
+                .putIfAbsent("My new name");
+
+        Set<String> animals = mSettingsPreference.animals().get();
+        for (String animal : animals) {
+            System.out.println("animal = " + animal);
+        }
+
+        float value = mSettingsPreference.amount().orElse(2f);
+
+        //Batch edition
+        mSettingsPreference.edit()
+                .count().put(1)
+                .timeTwo().remove()
+                .apply();
+    }
+}
